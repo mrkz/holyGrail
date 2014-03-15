13240b3ad7d77e8f5f2514f8d2c297b62cd9e430
Marcos Eleno Joaquín Simental Magaña <mrkzmrkz@gmail.com>
2014-03-15 17:14:18 -0600

    Añado ejercicio de funciones y variables globales.

---
+++ examples/funcs/global_vars.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+# Variables Globales
+# Deben ser especificadas en caso de ser utilizadas
+# localmente por alguna función
+
+foo = 14
+
+def deep_tought(foo = 999):
+	print "el valor de 'foo' es", foo
+	earth()
+
+def earth():
+	global foo
+	print "el valor de 'foo' es", foo
+
+def main():
+	"""Soy main, y esta es mi documentación"""
+	deep_tought()
+
+if __name__ == '__main__':
+	main()
---
+++ student/funcs/funciones.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+# Exercise 2
+#
+# Tu tarea consiste en crear una función que utilice la
+# variable global ans_life y la imprima el número de veces
+# de su valor; por ejemplo: si ans_life = 3, mostrarás
+# 3
+# 3
+# 3
+
+# Imposible Challange: Si el ejercicio 2 es muy sencillo para ti
+# esta será tu tarea: Crear una función que reciba un parametro
+# de llamado ans_life e imprimir dicha variable, además de imprimir la
+# variable global (cuyo valor es 42).
+# Hint: Mejor hazle caso al primer ejercicio (:
+
+ans_life = 42
+
+# declara tu funcion aquí
+
+
+def main():
+	# llama a tu función aquí
+
+
+if __name__ == '__main__':
+	main()
